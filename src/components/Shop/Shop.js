3fd6329361bba2e3fd335db47f9df11cae565c97
“T3sultan” <incognito.soul011@gmail.com>
2021-09-23 01:31:06 +0600

    dispaly data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 
@@ -17,7 +18,13 @@ const Shop = () => {
             <div className="product-container">
                 <h3>Products:{products.length}</h3>
                 {
-                    products.map(product => console.log(product))
+                    products.map(product => <Product
+                       key={product.key}
+                        product={product}
+
+                    >
+
+                    </Product>)
                 }
 
             </div>
