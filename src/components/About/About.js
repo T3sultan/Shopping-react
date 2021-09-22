9b3de461c4c6b0aa40009dafa7a81712f6f5c316
“T3sultan” <incognito.soul011@gmail.com>
2021-09-22 23:11:23 +0600

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css'
+
+const About = () => {
+    return (
+        <div>
+            <h1 className="title">About</h1>
+        </div>
+    );
+};
+
+export default About;
